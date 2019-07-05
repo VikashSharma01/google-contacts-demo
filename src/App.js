ddbf57540904a7474d16713a45b452796bf485d1
Vikash Sharma <vikash.sharma@mountblue.com>
2019-07-05 16:49:28 +0530

    Displayed all the list from Json file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DisplayContactList from './components/contacts';
 
 function App() {
+
   return (
+    <React.Fragment>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className= "contact-head">
+        <h3>My Contact List</h3>
+      </div>
+      <DisplayContactList />
     </div>
+    </React.Fragment>
   );
 }
 
